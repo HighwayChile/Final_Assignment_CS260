1473edc2ee4d92cdb41a545111af414d1c5c1a31
HighwayChile <slickdick101@gmail.com>
2024-06-10 22:32:14 -0700

    Here we go!

--- test_function.cpp
+++ test_function.cpp
@@ -1,19 +1,8 @@
-// #include <iostream>
-
-
-// #include "graph.h"
-// #include "vertex.h"
-// #include "edge.h"
-
-// using namespace std;
-
 
 #include <iostream>
 #include <vector>
 #include <limits>
 
-// #include "testor.cpp"
-// #include "test_function.cpp"
 #include "vertex.cpp"
 #include "edge.cpp"
 #include "graph.cpp"
@@ -28,7 +17,7 @@ void simple_graph_test() {
     cout << "Create Simple Graph" << endl;
     cout << "Creating Graph... " << endl;
     Graph simple_graph;
-    
+
     cout << "Adding nodes A, B, and C... " << endl;
     simple_graph.add_vertex("A");
     simple_graph.add_vertex("B");
@@ -45,7 +34,46 @@ void simple_graph_test() {
 
     simple_graph.print_graph();
 
+    simple_graph.~Graph();
+
 }
+
+
+void bin_tree_test() {
+    cout << "Create Binary Tree" << endl;
+    cout << "Creating tree... " << endl;
+    Graph simple_tree;
+
+    cout << "Adding nodes A, B, C, D, E, F, G, H and I... " << endl;
+    simple_tree.add_vertex("A");
+    simple_tree.add_vertex("B");
+    simple_tree.add_vertex("C");
+    simple_tree.add_vertex("D");
+    simple_tree.add_vertex("E");
+    simple_tree.add_vertex("F");
+    simple_tree.add_vertex("G");
+    simple_tree.add_vertex("H");
+    simple_tree.add_vertex("I");    
+
+    simple_tree.print_graph();
+
+
+    cout << "Adding unnamed edges... " << endl;
+    simple_tree.add_edge(5, simple_tree.find_vertex("A"), simple_tree.find_vertex("B"));
+    simple_tree.add_edge(4, simple_tree.find_vertex("A"), simple_tree.find_vertex("C"));
+    simple_tree.add_edge(2, simple_tree.find_vertex("B"), simple_tree.find_vertex("D"));
+    simple_tree.add_edge(2, simple_tree.find_vertex("B"), simple_tree.find_vertex("E"));
+    simple_tree.add_edge(2, simple_tree.find_vertex("C"), simple_tree.find_vertex("F"));
+    simple_tree.add_edge(2, simple_tree.find_vertex("C"), simple_tree.find_vertex("G"));
+    simple_tree.add_edge(2, simple_tree.find_vertex("G"), simple_tree.find_vertex("H"));
+    simple_tree.find_edge(simple_tree.find_vertex("G"), simple_tree.find_vertex("I"));
+
+    simple_tree.print_graph();
+
+    simple_tree.~Graph();
+}
+
+
 
 
 
@@ -53,6 +81,7 @@ void simple_graph_test() {
 int main() {
     // test_function();
     simple_graph_test();
+    bin_tree_test();
     // return 0;
     cout << endl;
     cout << "Quitting Program..." << endl;
